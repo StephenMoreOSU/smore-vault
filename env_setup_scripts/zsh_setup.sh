80e7f0e04e55cc5aa908885df6da1cd3180a007e
Stephen More <mores@oregonstate.edu>
2021-04-08 12:18:11 -0700

    environment setup scripts

---
+++ env_setup_scripts/zsh_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/agnoster/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
